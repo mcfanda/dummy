28e4b48c0258b1e52a7ae1d7fe3535a8c1e9cdc6
Marcello Gallucci <mcfanda@gmail.com>
2019-02-02 17:18:18 +0100

    new version

--- test.R
+++ test.R
@@ -8,10 +8,7 @@ change<-change+1
 change<-change+1
 change<-change+1
 change<-change+1
-<<<<<<< HEAD
 change<-change+1
 change<-change+1
 change<-change+1
-=======
->>>>>>> 425615f2504c846cf1d482f3af85302c57756628
 
